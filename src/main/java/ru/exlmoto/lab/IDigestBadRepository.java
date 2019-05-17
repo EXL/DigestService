e84582397dbbb61720d5c7928f4ad8e8f8740a66
EXL <exlmotodev@gmail.com>
2019-05-17 19:51:48 +0700

    Table migrating is done

--- src/main/java/ru/exlmoto/lab/IDigestBadRepository.java
+++
@@ -1,5 +0,0 @@
-package ru.exlmoto.lab;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface IDigestBadRepository extends CrudRepository<DigestBadEntity, Integer> { }
--- src/main/java/ru/exlmoto/lab/IDigestUsersBadRepository.java
+++
@@ -1,5 +0,0 @@
-package ru.exlmoto.lab;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface IDigestUsersBadRepository extends CrudRepository<DigestUserBadEntity, Integer> { }

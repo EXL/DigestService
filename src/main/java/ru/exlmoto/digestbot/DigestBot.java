a46a9ff7fdd3e5bde753a45863400fcd7111e222
EXL <exlmotodev@gmail.com>
2019-09-16 04:42:23 +0700

    Add Minimal working Telegram bot

--- src/main/java/ru/exlmoto/digestbot/DigestBot.java
+++ src/main/java/ru/exlmoto/digestbot/DigestBot.java
@@ -1,34 +1,42 @@
 package ru.exlmoto.digestbot;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 import java.util.List;
 
+@Component
 public class DigestBot extends TelegramLongPollingBot {
+    @Value("${digestbot.name}")
+    private String username;
 
-    private static int z = 0;
+    @Value("${digestbot.token}")
+    private String token;
+
+    @Value("${digestbot.max_updates}")
+    private int max_updates;
 
     @Override
     public void onUpdateReceived(Update update) {
-        System.out.println(z + " " + update.toString());
-        z++;
+        System.out.println(update.toString());
     }
 
     @Override
     public void onUpdatesReceived(List<Update> updates) {
-        if (updates.size() < 20) {
+        if (updates.size() < max_updates) {
             updates.forEach(this::onUpdateReceived);
         }
     }
 
     @Override
     public String getBotUsername() {
-        return null;
+        return username;
     }
 
     @Override
     public String getBotToken() {
-        return null;
+        return token;
     }
 }

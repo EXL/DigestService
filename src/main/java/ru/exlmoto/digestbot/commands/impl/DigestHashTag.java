b2996870160140c63240f4b10360eff550ba1084
EXL <exlmotodev@gmail.com>
2019-11-27 00:35:16 +0700

    Commit digests to DataBase

--- src/main/java/ru/exlmoto/digestbot/commands/impl/DigestHashTag.java
+++ src/main/java/ru/exlmoto/digestbot/commands/impl/DigestHashTag.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component;
 
 import ru.exlmoto.digestbot.DigestBot;
 import ru.exlmoto.digestbot.commands.BotCommand;
+import ru.exlmoto.digestbot.entities.DigestEntity;
 import ru.exlmoto.digestbot.utils.ReceivedMessage;
 import ru.exlmoto.digestbot.yaml.impl.YamlLocalizationHelper;
 
@@ -22,6 +23,16 @@ public class DigestHashTag extends BotCommand {
 			aDigestBot.sendSimpleMessage(aReceivedMessage.getChatId(), aReceivedMessage.getMessageId(),
 					aLocalizationHelper.getRandomLocalizedString("hashtag.digest.ok",
 							aReceivedMessage.getAvailableUsername()));
+
+			final DigestEntity lDigestEntity = new DigestEntity();
+			lDigestEntity.setAuthor(aReceivedMessage.getMessageUsernameId());
+			lDigestEntity.setDigest(lMessageText);
+			/// TODO: HTMLize text
+			lDigestEntity.setHtml(lMessageText);
+			lDigestEntity.setDate(aReceivedMessage.getMessageDate().longValue());
+			lDigestEntity.setChat(aReceivedMessage.getChatId());
+
+			aDigestBot.getIDigestEntriesRepository().save(lDigestEntity);
 		}
 	}
 }

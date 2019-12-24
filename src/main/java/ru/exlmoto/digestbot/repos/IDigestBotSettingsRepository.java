7b62461ef224c2de47304a658d8864f8f15641e0
EXL <exlmotodev@gmail.com>
2019-12-24 08:59:10 +0700

    Delete all code for huge refactoring

--- src/main/java/ru/exlmoto/digestbot/repos/IDigestBotSettingsRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.exlmoto.digestbot.repos;
-
-import org.springframework.data.repository.CrudRepository;
-
-import ru.exlmoto.digestbot.entities.DigestBotSettings;
-
-public interface IDigestBotSettingsRepository extends CrudRepository<DigestBotSettings, Integer> {
-    public DigestBotSettings findDigestBotSettingsById(final Integer aId);
-}
--- utils/ImportDigests/config_db.py
+++
@@ -1,7 +0,0 @@
-DB_USER = '<username>'
-DB_PASS = '<password>'
-DB_DATA = '<database name>'
-DB_HOST = '<hostname>'
-TG_API_ID = int('<telegram client api id>')
-TG_API_HASH = '<telegram client api hash>'
-MOTOFAN_CHAT = '-1001045117849'

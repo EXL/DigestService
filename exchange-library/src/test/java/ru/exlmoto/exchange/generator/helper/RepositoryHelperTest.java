998a66d0d3acd8cdfd770d6518a6e5800f2ca541
EXL <exlmotodev@gmail.com>
2020-01-10 13:37:43 +0700

    Move test from SpringBootTest to ContextConfiguration

--- exchange-library/src/test/java/ru/exlmoto/exchange/generator/helper/RepositoryHelperTest.java
+++ exchange-library/src/test/java/ru/exlmoto/exchange/generator/helper/RepositoryHelperTest.java
@@ -3,48 +3,54 @@ package ru.exlmoto.exchange.generator.helper;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.dao.InvalidDataAccessResourceUsageException;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
+import ru.exlmoto.exchange.repository.BankByRepository;
 import ru.exlmoto.exchange.repository.BankRuRepository;
+import ru.exlmoto.exchange.repository.BankUaRepository;
+import ru.exlmoto.exchange.repository.BankKzRepository;
 import ru.exlmoto.exchange.repository.MetalRuRepository;
 
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 import static org.mockito.Mockito.when;
 
-@SpringBootTest
-@ActiveProfiles("test")
+@SpringJUnitConfig
+@ContextConfiguration(classes = { RepositoryHelper.class })
 public class RepositoryHelperTest {
 	@Autowired
 	private RepositoryHelper repositoryHelper;
 
-	@MockBean private BankRuRepository bankRepository;
-	@MockBean private MetalRuRepository metalRepository;
+	@MockBean private BankRuRepository bankRuRepository;
+	@MockBean private BankUaRepository bankUaRepository;
+	@MockBean private BankByRepository bankByRepository;
+	@MockBean private BankKzRepository bankKzRepository;
+	@MockBean private MetalRuRepository metalRuRepository;
 
 	@Test
 	public void testBankRepositoryNull() {
-		when(bankRepository.getBankRu()).thenReturn(null);
+		when(bankRuRepository.getBankRu()).thenReturn(null);
 		assertNull(repositoryHelper.getBankRu());
 	}
 
 	@Test
 	public void testBankRepositoryException() {
-		when(bankRepository.getBankRu()).thenThrow(new InvalidDataAccessResourceUsageException("Test exception."));
+		when(bankRuRepository.getBankRu()).thenThrow(new InvalidDataAccessResourceUsageException("Test exception."));
 		assertNull(repositoryHelper.getBankRu());
 	}
 
 	@Test
 	public void testMetalRepositoryNull() {
-		when(metalRepository.getMetalRu()).thenReturn(null);
+		when(metalRuRepository.getMetalRu()).thenReturn(null);
 		assertNull(repositoryHelper.getMetalRu());
 	}
 
 	@Test
 	public void testMetalRepositoryException() {
-		when(metalRepository.getMetalRu()).thenThrow(new InvalidDataAccessResourceUsageException("Test exception."));
+		when(metalRuRepository.getMetalRu()).thenThrow(new InvalidDataAccessResourceUsageException("Test exception."));
 		assertNull(repositoryHelper.getMetalRu());
 	}
 }

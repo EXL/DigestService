d95ee7a0555f643a64860dcd4047f58e51ceacf4
EXL <exlmotodev@gmail.com>
2020-01-11 17:41:47 +0700

    Fix digest-site test

--- digest-site/src/test/java/ru/exlmoto/digest/site/controller/DigestRestControllerTest.java
+++ digest-site/src/test/java/ru/exlmoto/digest/site/controller/DigestRestControllerTest.java
@@ -5,10 +5,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import ru.exlmoto.exchange.ExchangeConfiguration;
+import ru.exlmoto.motofan.MotofanConfiguration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class DigestRestControllerTest {
 	@Autowired
 	private DigestRestController digestRestController;
@@ -18,7 +23,11 @@ public class DigestRestControllerTest {
 		assertThat(digestRestController).isNotNull();
 	}
 
-	@SpringBootApplication
+	@SpringBootApplication(scanBasePackageClasses = {
+		ExchangeConfiguration.class,
+		MotofanConfiguration.class,
+		DigestRestController.class
+	})
 	public static class DigestSiteConfigurationCommon {
 
 	}

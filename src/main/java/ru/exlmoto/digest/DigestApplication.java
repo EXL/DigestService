e719cf698d2d729402c5a47c24623c7fab3aa69f
EXL <exlmotodev@gmail.com>
2020-01-13 18:52:17 +0700

    Fix markup

--- src/main/java/ru/exlmoto/digest/DigestApplication.java
+++ src/main/java/ru/exlmoto/digest/DigestApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DigestApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DigestApplication.class, args);
 	}
-
 }
--- src/test/java/ru/exlmoto/digest/DigestApplicationTests.java
+++ src/test/java/ru/exlmoto/digest/DigestApplicationTests.java
@@ -1,13 +1,19 @@
 package ru.exlmoto.digest;
 
 import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class DigestApplicationTests {
+	@Autowired
+	private DigestApplication application;
 
 	@Test
 	void contextLoads() {
+		assertNotNull(application);
 	}
-
 }

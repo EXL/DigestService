b92ab9e256978c9fce91432d6453aa7d47b3aa22
EXL <exlmotodev@gmail.com>
2020-01-13 18:42:44 +0700

    Add spring skeleton structure

---
+++ src/main/java/ru/exlmoto/digest/DigestApplication.java
@@ -0,0 +1,13 @@
+package ru.exlmoto.digest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/exlmoto/digest/DigestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.exlmoto.digest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

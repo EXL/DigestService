8a79ec0edfcc0b81f22eec133de7133059ff95fc
EXL <exlmotodev@gmail.com>
2020-01-15 20:22:27 +0700

    Add Resource helper class and test

---
+++ src/main/java/ru/exlmoto/digest/util/resource/ResourceHelper.java
@@ -0,0 +1,45 @@
+package ru.exlmoto.digest.util.resource;
+
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
+import org.springframework.util.FileCopyUtils;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UncheckedIOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+/*
+ * Source: https://www.baeldung.com/spring-load-resource-as-string
+ * Why is used `StandardCharsets.UTF_8` instead of `Charset.defaultCharset().name()`?
+ * Perhaps second one will not work as excepted on Windows OS.
+ */
+
+@Component
+public class ResourceHelper {
+	public String asString(Resource resource) {
+		return asString(resource, StandardCharsets.UTF_8);
+	}
+
+	public String readFileToString(String path) {
+		return readFileToString(path, StandardCharsets.UTF_8);
+	}
+
+	public String asString(Resource resource, Charset charset) {
+		try (Reader reader = new InputStreamReader(resource.getInputStream(), charset)) {
+			return FileCopyUtils.copyToString(reader);
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
+	}
+
+	public String readFileToString(String path, Charset charset) {
+		ResourceLoader resourceLoader = new DefaultResourceLoader();
+		Resource resource = resourceLoader.getResource(path);
+		return asString(resource, charset);
+	}
+}
---
+++ src/test/java/ru/exlmoto/digest/util/resource/ResourceHelperTest.java
@@ -0,0 +1,45 @@
+package ru.exlmoto.digest.util.resource;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.Resource;
+
+import java.io.UncheckedIOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+class ResourceHelperTest {
+	@Value("classpath:chart/charts.yml")
+	private Resource resourceOk;
+
+	@Value("classpath:unknown-file.ext")
+	private Resource resourceFail;
+
+	@Autowired
+	private ResourceHelper resourceHelper;
+
+	@Test
+	public void testReadFileToString() {
+		String res = resourceHelper.readFileToString("classpath:chart/charts.yml");
+		assertThat(res).isNotEmpty();
+		System.out.println(res.substring(0, 40));
+
+		assertThrows(UncheckedIOException.class,
+			() -> resourceHelper.readFileToString("classpath:unknown-file.ext"));
+	}
+
+	@Test
+	public void testAsString() {
+		String res = resourceHelper.asString(resourceOk);
+		assertThat(res).isNotEmpty();
+		System.out.println(res.substring(0, 40));
+
+		assertThrows(UncheckedIOException.class, () -> resourceHelper.asString(resourceFail));
+	}
+}

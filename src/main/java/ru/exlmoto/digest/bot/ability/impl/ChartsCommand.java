74a48a471ce5bfeeb8b349a5cba970edfbf3b397
EXL <exlmotodev@gmail.com>
2020-01-27 21:25:20 +0700

    Add strings and chart service to chart command

--- src/main/java/ru/exlmoto/digest/bot/ability/impl/ChartsCommand.java
+++ src/main/java/ru/exlmoto/digest/bot/ability/impl/ChartsCommand.java
@@ -8,18 +8,21 @@ import ru.exlmoto.digest.bot.ability.BotAbility;
 import ru.exlmoto.digest.bot.keyboard.impl.ChartsKeyboard;
 import ru.exlmoto.digest.bot.sender.BotSender;
 import ru.exlmoto.digest.bot.util.BotHelper;
+import ru.exlmoto.digest.chart.ChartService;
 import ru.exlmoto.digest.util.i18n.LocalizationHelper;
 
 @Component
 public class ChartsCommand extends BotAbility {
-	private final ChartsKeyboard chartsKeyboard;
+	private final ChartsKeyboard keyboard;
+	private final ChartService service;
 
-	public ChartsCommand(ChartsKeyboard chartsKeyboard) {
-		this.chartsKeyboard = chartsKeyboard;
+	public ChartsCommand(ChartsKeyboard keyboard, ChartService service) {
+		this.keyboard = keyboard;
+		this.service = service;
 	}
 
 	@Override
 	protected void execute(BotHelper helper, BotSender sender, LocalizationHelper locale, Message message) {
-		sender.replyKeyboard(message.chat().id(), message.messageId(), "Test!", chartsKeyboard.getMarkup());
+		sender.replyKeyboard(message.chat().id(), message.messageId(), "Test!", keyboard.getMarkup());
 	}
 }

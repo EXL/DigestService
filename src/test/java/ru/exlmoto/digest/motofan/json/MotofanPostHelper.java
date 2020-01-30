9b9591a9973b63511fb06eafb866f38949e05d50
EXL <exlmotodev@gmail.com>
2020-01-30 16:51:57 +0700

    Move to another random generation class

--- src/test/java/ru/exlmoto/digest/motofan/json/MotofanPostHelper.java
+++ src/test/java/ru/exlmoto/digest/motofan/json/MotofanPostHelper.java
@@ -1,20 +1,20 @@
 package ru.exlmoto.digest.motofan.json;
 
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class MotofanPostHelper {
 	public MotofanPost getRandomMotofanPost(Long timestamp) {
 		return
 			new MotofanPost(
 				timestamp,
-				String.valueOf(new Random().nextLong()),
-				new Random().nextLong(),
-				new Random().nextLong(),
-				String.valueOf(new Random().nextLong()),
-				String.valueOf(new Random().nextLong()),
-				String.valueOf(new Random().nextLong()),
-				String.valueOf(new Random().nextLong()),
-				String.valueOf(new Random().nextLong())
+				String.valueOf(ThreadLocalRandom.current().nextLong()),
+				ThreadLocalRandom.current().nextLong(),
+				ThreadLocalRandom.current().nextLong(),
+				String.valueOf(ThreadLocalRandom.current().nextLong()),
+				String.valueOf(ThreadLocalRandom.current().nextLong()),
+				String.valueOf(ThreadLocalRandom.current().nextLong()),
+				String.valueOf(ThreadLocalRandom.current().nextLong()),
+				String.valueOf(ThreadLocalRandom.current().nextLong())
 			);
 	}
 }

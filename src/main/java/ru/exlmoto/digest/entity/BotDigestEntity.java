e1896d607edc2ce23528de47f644add4ecb3e9e7
EXL <exlmotodev@gmail.com>
2020-02-05 21:21:22 +0700

    Use GenerationType.IDENTITY instead of GenerationType.AUTO

--- src/main/java/ru/exlmoto/digest/entity/BotDigestEntity.java
+++ src/main/java/ru/exlmoto/digest/entity/BotDigestEntity.java
@@ -13,7 +13,7 @@ import javax.persistence.Table;
 @Table(name = "bot_digest")
 public class BotDigestEntity {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 
 	@Column(nullable = false)

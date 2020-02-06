6c690b8f211f00fa6f0172c16cd24d0fbdeec5d8
EXL <exlmotodev@gmail.com>
2020-02-06 17:25:20 +0700

    Add some filters methods

--- src/test/java/ru/exlmoto/digest/util/filter/FilterHelperUnitTest.java
+++ src/test/java/ru/exlmoto/digest/util/filter/FilterHelperUnitTest.java
@@ -6,38 +6,55 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class FilterHelperUnitTest {
-	private final FilterHelper filterHelper = new FilterHelper();
+	private final FilterHelper filter = new FilterHelper();
 
 	@Test
 	public void testRemoveHtmlTags() {
-		assertThrows(NullPointerException.class, () -> filterHelper.removeHtmlTags(null));
+		assertThrows(NullPointerException.class, () -> filter.removeHtmlTags(null));
 
-		assertEquals("test text test", filterHelper.removeHtmlTags("test <b>text</b> test"));
-		assertEquals("test text test", filterHelper.removeHtmlTags("test <b id=\"a\">text</b> test"));
-		assertEquals("test text test", filterHelper.removeHtmlTags("test <asdasd> text test</..."));
-		assertEquals("test text test", filterHelper.removeHtmlTags("test <blockquote> text test</block"));
+		assertEquals("test text test", filter.removeHtmlTags("test <b>text</b> test"));
+		assertEquals("test text test", filter.removeHtmlTags("test <b id=\"a\">text</b> test"));
+		assertEquals("test text test", filter.removeHtmlTags("test <asdasd> text test</..."));
+		assertEquals("test text test", filter.removeHtmlTags("test <blockquote> text test</block"));
 	}
 
 	@Test
 	public void testRemoveBbTags() {
-		assertThrows(NullPointerException.class, () -> filterHelper.removeBbCodes(null));
+		assertThrows(NullPointerException.class, () -> filter.removeBbCodes(null));
 
-		assertEquals("test text test", filterHelper.removeBbCodes("test [b]text[/b] test"));
-		assertEquals("test text test bl", filterHelper.removeBbCodes("test [b]text[/b] test [bl"));
-		assertEquals("test text test bl", filterHelper.removeBbCodes("test [b]text[/b] test [\\bl"));
+		assertEquals("test text test", filter.removeBbCodes("test [b]text[/b] test"));
+		assertEquals("test text test bl", filter.removeBbCodes("test [b]text[/b] test [bl"));
+		assertEquals("test text test bl", filter.removeBbCodes("test [b]text[/b] test [\\bl"));
 	}
 
 	@Test
 	public void testCheckLink() {
-		assertEquals("https://t.me/", filterHelper.checkLink("https://t.me"));
-		assertEquals("https://t.me/", filterHelper.checkLink("https://t.me/"));
+		assertEquals("https://t.me/", filter.checkLink("https://t.me"));
+		assertEquals("https://t.me/", filter.checkLink("https://t.me/"));
 	}
 
 	@Test
 	public void testGetDateFromTimeStamp() {
-		System.out.println(filterHelper.getDateFromTimeStamp("dd.MM.yyyy HH:mm", 1580520624L));
-		System.out.println(filterHelper.getDateFromTimeStamp("dd.MMM.yyyy HH:mm", 1580520624L));
-		System.out.println(filterHelper.getDateFromTimeStamp("dd-MM-yyyy HH:mm", 1580585762L));
-		System.out.println(filterHelper.getDateFromTimeStamp("dd-MMM-yyyy HH:mm", 1580585762L));
+		System.out.println(filter.getDateFromTimeStamp("dd.MM.yyyy HH:mm", 1580520624L));
+		System.out.println(filter.getDateFromTimeStamp("dd.MMM.yyyy HH:mm", 1580520624L));
+		System.out.println(filter.getDateFromTimeStamp("dd-MM-yyyy HH:mm", 1580585762L));
+		System.out.println(filter.getDateFromTimeStamp("dd-MMM-yyyy HH:mm", 1580585762L));
+	}
+
+	@Test
+	public void testStrip() {
+		assertEquals(
+			"exlmoto test@test ada@adasd.ru azxczxz asd zc@ adsa@ asdas (exlmoto)",
+			filter.strip(
+				filter.removeUserCasts(
+					"\n@exlmoto\ttest@test ada@adasd.ru @azxczxz asd zc@ adsa@ @ asdas (@exlmoto)\n\n")
+			)
+		);
+	}
+
+	@Test
+	public void testRemoveUserCasts() {
+		assertEquals("exlmoto test@test ada@adasd.ru azxczxz asd zc@ adsa@  asdas (exlmoto)",
+			filter.removeUserCasts("@exlmoto test@test ada@adasd.ru @azxczxz asd zc@ adsa@ @ asdas (@exlmoto)"));
 	}
 }

b9cfad51cc954c3ae874db0f8e943538e5fdcdba
EXL <exlmotodev@gmail.com>
2020-03-18 01:34:27 +0700

    Add additional tests for site helper class
    Rewrite some methods

--- src/main/java/ru/exlmoto/digest/site/controller/SiteController.java
+++ src/main/java/ru/exlmoto/digest/site/controller/SiteController.java
@@ -12,8 +12,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import org.thymeleaf.util.ArrayUtils;
-
 import ru.exlmoto.digest.bot.util.BotHelper;
 import ru.exlmoto.digest.entity.BotDigestEntity;
 import ru.exlmoto.digest.entity.BotDigestUserEntity;
@@ -32,9 +30,6 @@ import ru.exlmoto.digest.util.i18n.LocaleHelper;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.StringJoiner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @Controller
 public class SiteController {
@@ -49,24 +44,11 @@ public class SiteController {
 	private final SiteHelper siteHelper;
 	private final FilterHelper filter;
 
-	private enum Group {
-		Guest,
-		User,
-		Moderator,
-		Administrator
-	}
-
-	@Value("${general.date-format}")
-	private String dateFormat;
-
 	@Value("${bot.motofan-chat-id}")
 	private long motofanChatId;
 
 	@Value("${bot.motofan-chat-url}")
 	private String motofanChatUrl;
-
-	@Value("${bot.telegram-short-url}")
-	private String telegramShortUrl;
 
 	public SiteController(SiteConfiguration config,
 	                      BotDigestRepository repository,
@@ -251,7 +233,7 @@ public class SiteController {
 	protected List<Post> getDigestEntities(Page<BotDigestEntity> digestEntities,
 	                                       String postId,
 	                                       int current,
-	                                       String text,
+	                                       String search,
 	                                       Locale lang) {
 		if (digestEntities != null) {
 			List<Post> posts = new ArrayList<>();
@@ -259,184 +241,25 @@ public class SiteController {
 			for (BotDigestEntity digest : digestEntities) {
 				BotDigestUserEntity user = digest.getUser();
 				String username = user.getUsername();
+				long userId = user.getId();
 				long id = digest.getId();
 				posts.add(
 					new Post(
-						highlightPost(postId, id),
-						filterDescription(++count, id, digest.getMessageId(), lang),
+						siteHelper.highlightPost(postId, id),
+						siteHelper.filterDescription(++count, id, digest.getMessageId(), lang),
 						username,
-						filterUsername(username, false),
-						filterAvatarLink(user.getAvatar()),
-						filterGroup(username, lang),
-						filterDateAndTime(digest.getDate(), lang),
-						activateUsers(activateLinks(filterHighlight(digest.getDigest(), text))),
-						user.getId(),
-						filterDigestCount(user, lang)
+						siteHelper.filterUsername(username, false),
+						siteHelper.filterAvatarLink(user.getAvatar()),
+						siteHelper.filterGroup(username, lang),
+						siteHelper.filterDateAndTime(digest.getDate(), lang),
+						siteHelper.filterDigestOrder(digest.getDigest(), search),
+						userId,
+						siteHelper.filterDigestCount(userId, repository.countBotDigestEntitiesByUserEqualsAndChatEquals(user, motofanChatId), lang)
 					)
 				);
 			}
 			return posts;
 		}
 		return new ArrayList<>();
-	}
-
-	private String filterHighlight(String message, String highlight) {
-		if (highlight == null || highlight.isEmpty()) {
-			return message;
-		}
-		String[] words = message.split("\\s+");
-		StringJoiner joiner = new StringJoiner(" ");
-		for (String word : words) {
-			String trim = word.trim();
-			if (!trim.isEmpty()) {
-				joiner.add(checkLinkAndUsername(word, highlight));
-			}
-		}
-		return joiner.toString();
-	}
-
-	private String checkLinkAndUsername(String word, String highlight) {
-		String wordCase = word.toLowerCase();
-		String highlightCase = highlight.toLowerCase();
-
-		if (wordCase.contains(highlightCase) && !word.contains("@") && !word.contains("://")) {
-			StringBuilder stringBuffer = new StringBuilder(word);
-			int start = wordCase.indexOf(highlightCase);
-			int end = start + highlight.length();
-			String text = word.substring(start, end);
-			stringBuffer.replace(start, end, "<span class=\"text-highlight\">" + text + "</span>");
-			return stringBuffer.toString();
-		}
-		return word;
-	}
-
-	private String filterDigestCount(BotDigestUserEntity user, Locale lang) {
-		return String.format(locale.i18nW("site.content.user.digests", lang),
-			filter.checkLink(config.getAddress()) + "search?user=" + user.getId(),
-			repository.countBotDigestEntitiesByUserEqualsAndChatEquals(user, motofanChatId));
-	}
-
-	protected String filterDescription(int count, long id, Long messageId, Locale lang) {
-		String link = String.format(locale.i18nW("site.content.description", lang),
-			filter.checkLink(config.getAddress()) + "jump?id=" + id, count, id);
-		if (messageId != null) {
-			return String.format(locale.i18nW("site.content.chat.link", lang),
-				filter.checkLink(motofanChatUrl) + messageId) + link;
-		}
-		return link;
-	}
-
-	protected String activateUsers(String digest) {
-		Matcher matcher = Pattern.compile("\\B@[a-z0-9_-]+",
-			Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL).matcher(digest);
-		StringBuffer stringBuffer = new StringBuffer();
-		while (matcher.find()) {
-			matcher.appendReplacement(stringBuffer, filterUsername(matcher.group(0).trim(), true));
-		}
-		matcher.appendTail(stringBuffer);
-		return stringBuffer.toString();
-	}
-
-	// https://stackoverflow.com/a/28269120
-	protected String activateLinks(String digest) {
-		final int length = 100;
-		Matcher matcher =
-			Pattern.compile("((https?|ftp|gopher|telnet|file):((//)|(\\\\))+[\\w\\d:#@%/;$()~_?\\+-=\\\\\\.&]*)",
-			Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL).matcher(digest);
-		StringBuffer stringBuffer = new StringBuffer();
-		while (matcher.find()) {
-			String url = matcher.group(0).trim();
-			String shortUrl = filter.ellipsisMiddle(url, length);
-			matcher.appendReplacement(stringBuffer,
-				String.format("<a href=\"%1$s\" title=\"%1$s\" target=\"_blank\">%2$s</a>", url, shortUrl));
-		}
-		matcher.appendTail(stringBuffer);
-		return stringBuffer.toString();
-	}
-
-	protected String filterUsername(String username, boolean at) {
-		switch (checkGroup(username)) {
-			default:
-			case Guest: {
-				return filter.ellipsisRight(username, 20);
-			}
-			case User: {
-				return getUsernameLink(username, "member-user", at);
-			}
-			case Moderator: {
-				return getUsernameLink(username, "member-moderator", at);
-			}
-			case Administrator: {
-				return getUsernameLink(username, "member-administrator", at);
-			}
-		}
-	}
-
-	protected String filterGroup(String username, Locale lang) {
-		switch (checkGroup(username)) {
-			default:
-			case Guest: {
-				return locale.i18nW("site.content.group.guest", lang);
-			}
-			case User: {
-				return locale.i18nW("site.content.group.user", lang);
-			}
-			case Moderator: {
-				return locale.i18nW("site.content.group.moderator", lang);
-			}
-			case Administrator: {
-				return locale.i18nW("site.content.group.administrator", lang);
-			}
-		}
-	}
-
-	protected String filterDateAndTime(long timestamp, Locale lang) {
-		String[] dateAndTime = filter.getDateFromTimeStamp(dateFormat, timestamp).split(" ");
-		if (dateAndTime.length == 2) {
-			return String.format(locale.i18nW("site.content.date.time", lang), dateAndTime[0], dateAndTime[1]);
-		}
-		return locale.i18nW("site.content.date.time.wrong", lang);
-	}
-
-	protected String getUsernameLink(String username, String className, boolean at) {
-		String usernameWithoutAt = dropAt(username);
-		String name = (at) ? username : usernameWithoutAt;
-		return String.format("<a href=\"%s\" title=\"%s\" target=\"_blank\"><span class=\"%s\">%s</span></a>",
-			filter.checkLink(telegramShortUrl) + usernameWithoutAt, username, className,
-			filter.ellipsisRight(name, 20));
-	}
-
-	protected Group checkGroup(String username) {
-		if (botHelper.isUserAdmin(dropAt(username))) {
-			return Group.Administrator;
-		} else if (ArrayUtils.contains(config.getModerators(), dropAt(username))) {
-			return Group.Moderator;
-		} else if (username.startsWith("@")) {
-			return Group.User;
-		}
-		return Group.Guest;
-	}
-
-	protected String dropAt(String username) {
-		if (username.startsWith("@")) {
-			return username.substring(1);
-		}
-		return username;
-	}
-
-	protected String filterAvatarLink(String avatarLink) {
-		if (avatarLink != null) {
-			if (config.isProxyEnabled()) {
-				return filter.checkLink(config.getProxy()) + avatarLink.substring(avatarLink.indexOf("://") + 3);
-			} else {
-				return avatarLink;
-			}
-		}
-		return null;
-	}
-
-	protected boolean highlightPost(String postId, long id) {
-		Long post = siteHelper.getLong(postId);
-		return (post != null && post.equals(id));
 	}
 }

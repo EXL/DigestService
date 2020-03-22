97dc2dea29ae0ecb438585133efe59467b83a9a8
EXL <exlmotodev@gmail.com>
2020-03-22 14:25:01 +0700

    Fix and add some tests

--- src/test/java/ru/exlmoto/digest/bot/handler/BotHandlerMockTest.java
+++ src/test/java/ru/exlmoto/digest/bot/handler/BotHandlerMockTest.java
@@ -2,24 +2,29 @@ package ru.exlmoto.digest.bot.handler;
 
 import org.junit.jupiter.api.Test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import ru.exlmoto.digest.bot.util.UpdateHelper;
+import ru.exlmoto.digest.service.DatabaseService;
 
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest(properties = "bot.silent=true")
 public class BotHandlerMockTest {
-	@SpyBean
+	@Autowired
 	private BotHandler handler;
+
+	@MockBean
+	private DatabaseService service;
 
 	private final UpdateHelper update = new UpdateHelper();
 
 	@Test
 	public void testDisableGreetingsByGroup() {
-		when(handler.checkGreetingStatus(anyLong())).thenReturn(false);
+		when(service.checkGreeting(anyLong())).thenReturn(false);
 
 		/* No output. */
 		handler.onNewUsers(update.getNewUsers(4));

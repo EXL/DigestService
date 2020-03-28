13700a27d3c37d5867dcd0d1f461904261e7a17b
EXL <exlmotodev@gmail.com>
2020-03-28 23:40:04 +0700

    Add Telegram html generator

--- src/main/java/ru/exlmoto/digest/covid/CovidService.java
+++ src/main/java/ru/exlmoto/digest/covid/CovidService.java
@@ -3,16 +3,23 @@ package ru.exlmoto.digest.covid;
 import org.springframework.stereotype.Service;
 
 import ru.exlmoto.digest.covid.generator.CovidJsonGenerator;
+import ru.exlmoto.digest.covid.generator.CovidTgHtmlGenerator;
 
 @Service
 public class CovidService {
 	private final CovidJsonGenerator jsonGenerator;
+	private final CovidTgHtmlGenerator htmlGenerator;
 
-	public CovidService(CovidJsonGenerator jsonGenerator) {
+	public CovidService(CovidJsonGenerator jsonGenerator, CovidTgHtmlGenerator htmlGenerator) {
 		this.jsonGenerator = jsonGenerator;
+		this.htmlGenerator = htmlGenerator;
 	}
 
 	public String jsonReport() {
 		return jsonGenerator.getJsonReport();
+	}
+
+	public String tgHtmlReport() {
+		return htmlGenerator.getTgHtmlReport();
 	}
 }
---
+++ src/main/java/ru/exlmoto/digest/covid/generator/CovidTgHtmlGenerator.java
@@ -0,0 +1,102 @@
+package ru.exlmoto.digest.covid.generator;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.util.Pair;
+import org.springframework.stereotype.Component;
+
+import ru.exlmoto.digest.covid.json.Region;
+import ru.exlmoto.digest.covid.parser.Covid2GisParser;
+import ru.exlmoto.digest.util.Answer;
+import ru.exlmoto.digest.util.filter.FilterHelper;
+import ru.exlmoto.digest.util.i18n.LocaleHelper;
+
+import java.time.Instant;
+
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class CovidTgHtmlGenerator {
+	private final Covid2GisParser parser;
+	private final LocaleHelper locale;
+	private final FilterHelper filter;
+
+	@Value("${covid.date-format}")
+	private String dateFormat;
+
+	public CovidTgHtmlGenerator(Covid2GisParser parser, LocaleHelper locale, FilterHelper filter) {
+		this.parser = parser;
+		this.locale = locale;
+		this.filter = filter;
+	}
+
+	public String getTgHtmlReport() {
+		Answer<Pair<List<Region>, Map<String, String>>> res = parser.parse2GisData();
+		if (res.ok()) {
+			Pair<List<Region>, Map<String, String>> answer = res.answer();
+			return generateTgHtmlReport(answer.getFirst(), answer.getSecond());
+		} else {
+			return String.format(locale.i18n("covid.error"), res.error());
+		}
+	}
+
+	private String generateTgHtmlReport(List<Region> cases, Map<String, String> history) {
+		String report = generateHeader(history);
+		report += generateTable(cases);
+		return report;
+	}
+
+	private String generateHeader(Map<String, String> history) {
+		String head = locale.i18n("covid.head");
+		head += "\n\n";
+		head += String.format(locale.i18n("covid.head.cases"), history.get("cases")) +
+			getDiff(history.get("cases_diff")) + "\n";
+		head += String.format(locale.i18n("covid.head.recover"), history.get("recover")) +
+			getDiff(history.get("recover_diff")) + "\n";
+		head += String.format(locale.i18n("covid.head.deaths"), history.get("deaths")) +
+			getDiff(history.get("deaths_diff")) + "\n";
+		head += String.format(locale.i18n("covid.head.date"), getDateFormat(history.get("date"))) + "\n";
+		head += "\n";
+		head += locale.i18n("covid.head.home");
+		return head;
+	}
+
+	private String generateTable(List<Region> cases) {
+		final int CHOP_NUMBER = 5;
+		final int CHOP_CASES = 10;
+		final int CHOP_REGION = 20;
+
+		StringBuilder builder = new StringBuilder("\n\n<pre>\n");
+		builder.append(filter.arrangeString(locale.i18n("covid.table.region"), CHOP_REGION)).append(" ");
+		builder.append(filter.arrangeString(locale.i18n("covid.table.cases"), CHOP_CASES)).append(" ");
+		builder.append(filter.arrangeString(locale.i18n("covid.table.recover"), CHOP_NUMBER)).append(" ");
+		builder.append(filter.arrangeString(locale.i18n("covid.table.deaths"), CHOP_NUMBER)).append("\n");
+
+		for (int i = 0; i < CHOP_NUMBER * 2 + CHOP_REGION + CHOP_CASES + 3; ++i) {
+			builder.append("-");
+		}
+		builder.append("\n");
+
+		for (Region report : cases) {
+			builder.append(filter.ellipsisRightA(report.getName(), CHOP_REGION)).append(" ");
+			builder.append(filter.ellipsisRightA(report.getCases() +
+				getDiff(String.valueOf(report.getDiff())), CHOP_CASES)).append(" ");
+			builder.append(filter.ellipsisRightA(String.valueOf(report.getRecover()), CHOP_NUMBER)).append(" ");
+			builder.append(filter.ellipsisRightA(String.valueOf(report.getDeaths()), CHOP_NUMBER)).append("\n");
+		}
+		builder.append("</pre>");
+
+		return builder.toString();
+	}
+
+	private String getDateFormat(String date) {
+		return filter.getDateFromTimeStamp(dateFormat, Instant.parse(date).getEpochSecond());
+	}
+
+	private String getDiff(String diff) {
+		if (!diff.equals("0") && !diff.startsWith("-")) {
+			return " " + String.format(locale.i18n("covid.increase"), diff);
+		}
+		return "";
+	}
+}

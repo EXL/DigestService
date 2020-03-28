3904a12195d6d856bbb50738e831c02ad8e612d1
EXL <exlmotodev@gmail.com>
2020-03-28 21:32:47 +0700

    Add COVID-19 service

---
+++ src/main/java/ru/exlmoto/digest/covid/json/Region.java
@@ -0,0 +1,85 @@
+package ru.exlmoto.digest.covid.json;
+
+public class Region {
+	private String name;
+
+	private long cases;
+	private long recover;
+	private long deaths;
+
+	private long diff;
+	private long sick; // Always zero?
+
+	public Region() {
+
+	}
+
+	public Region(String name, long cases, long recover, long deaths, long diff, long sick) {
+		this.name = name;
+		this.cases = cases;
+		this.recover = recover;
+		this.deaths = deaths;
+		this.diff = diff;
+		this.sick = sick;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public long getCases() {
+		return cases;
+	}
+
+	public void setCases(long cases) {
+		this.cases = cases;
+	}
+
+	public long getRecover() {
+		return recover;
+	}
+
+	public void setRecover(long recover) {
+		this.recover = recover;
+	}
+
+	public long getDeaths() {
+		return deaths;
+	}
+
+	public void setDeaths(long deaths) {
+		this.deaths = deaths;
+	}
+
+	public long getDiff() {
+		return diff;
+	}
+
+	public void setDiff(long diff) {
+		this.diff = diff;
+	}
+
+	public long getSick() {
+		return sick;
+	}
+
+	public void setSick(long sick) {
+		this.sick = sick;
+	}
+
+	@Override
+	public String toString() {
+		return
+			"Region{name=" + name +
+			", cases=" + cases +
+			", recover=" + recover +
+			", deaths=" + deaths +
+			", diff=" + diff +
+			", sick=" + sick +
+			"}";
+	}
+}

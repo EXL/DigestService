98238d0d4d3a1acd1daa937a42ffb36db1e13d32
EXL <exlmotodev@gmail.com>
2020-03-29 17:35:21 +0700

    Add tests for COVID-2019 generator

---
+++ src/test/java/ru/exlmoto/digest/covid/generator/CovidJsonGeneratorTest.java
@@ -0,0 +1,64 @@
+package ru.exlmoto.digest.covid.generator;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+import ru.exlmoto.digest.util.file.ResourceHelper;
+import ru.exlmoto.digest.util.rest.RestHelper;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.mockito.Mockito.when;
+
+import static ru.exlmoto.digest.util.Answer.Ok;
+
+@SpringBootTest
+class CovidJsonGeneratorTest {
+	@Autowired
+	private CovidJsonGenerator generator;
+
+	@SpyBean
+	private RestHelper rest;
+
+	@Autowired
+	private ResourceHelper helper;
+
+	@Value("${covid.url}")
+	private String covidUrl;
+
+	@Test
+	public void testGetJsonReport() {
+		when(rest.getRestResponse(covidUrl + "/public/13.js"))
+			.thenReturn(Ok(helper.readFileToString("classpath:covid/13-next.js", StandardCharsets.ISO_8859_1)));
+		when(rest.getRestResponse(covidUrl + "/public/14.js"))
+			.thenReturn(Ok(helper.readFileToString("classpath:covid/14-next.js")));
+		when(rest.getRestResponse(covidUrl + "/public/22.js"))
+			.thenReturn(Ok(helper.readFileToString("classpath:covid/22-next.js")));
+
+		String report = generator.getJsonReport(covidUrl);
+		assertTrue(report.startsWith("{"));
+
+		System.out.println(report);
+	}
+
+	@Test
+	public void testGetJsonReportOnError() {
+		when(rest.getRestResponse(covidUrl + "/public/13.js"))
+			.thenReturn(Ok(helper.readFileToString("classpath:covid/13-broken.js")));
+		when(rest.getRestResponse(covidUrl + "/public/14.js"))
+			.thenReturn(Ok(helper.readFileToString("classpath:covid/14-broken.js")));
+		when(rest.getRestResponse(covidUrl + "/public/22.js"))
+			.thenReturn(Ok(helper.readFileToString("classpath:covid/22-broken.js")));
+
+		String report = generator.getJsonReport(covidUrl);
+		assertTrue(report.startsWith("{"));
+
+		System.out.println(report);
+	}
+}

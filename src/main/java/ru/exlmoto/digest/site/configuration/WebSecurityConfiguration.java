8ad9207d4702195e0ecc01cf30707d2cf417bffa
EXL <exlmotodev@gmail.com>
2020-04-02 04:50:54 +0700

    Add login form

--- src/main/java/ru/exlmoto/digest/site/configuration/WebSecurityConfiguration.java
+++ src/main/java/ru/exlmoto/digest/site/configuration/WebSecurityConfiguration.java
@@ -1,12 +1,18 @@
 package ru.exlmoto.digest.site.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
@@ -15,8 +21,8 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected UserDetailsService userDetailsService() {
 		UserDetails user = User.withDefaultPasswordEncoder()
-			.username("user")
-			.password("pass")
+			.username("u")
+			.password("p")
 			.roles("ADMIN")
 			.build();
 		return new InMemoryUserDetailsManager(user);
@@ -25,10 +31,20 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-			.antMatchers("/obey/**").authenticated()
-			.anyRequest().permitAll().and()
-			.formLogin().permitAll().and()
-			.logout().permitAll();
+			.antMatchers("/obey/**")
+				.authenticated()
+			.anyRequest()
+				.permitAll()
+			.and()
+				.formLogin()
+				.loginPage("/ds-auth-login")
+				.defaultSuccessUrl("/obey")
+				.failureForwardUrl("/ds-auth-login?error=true")
+			.and()
+				.logout()
+				.logoutUrl("/ds-auth-logout")
+				.logoutSuccessUrl("/")
+				.invalidateHttpSession(true);
 
 		/*
 		http
@@ -42,6 +58,30 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 			.and()
 			.logout()
 			.permitAll();*/
+	}
+/*
+	public SimpleAuthenticationFilter authenticationFilter() throws Exception {
+		SimpleAuthenticationFilter filter = new SimpleAuthenticationFilter();
+		filter.setAuthenticationManager(authenticationManagerBean());
+		filter.setAuthenticationFailureHandler(failureHandler());
+		return filter;
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(authProvider());
+	}
+
+
+	public AuthenticationProvider authProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailsService);
+		provider.setPasswordEncoder(passwordEncoder());
+		return provider;
+	}
+
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 	/*

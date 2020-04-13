267b7f7ef927fc633774adfee0f76a7102d35083
EXL <exlmotodev@gmail.com>
2020-04-14 01:27:08 +0700

    Drop additional field test

--- src/main/java/ru/exlmoto/digest/site/configuration/WebSecurityConfiguration.java
+++ src/main/java/ru/exlmoto/digest/site/configuration/WebSecurityConfiguration.java
@@ -1,26 +1,21 @@
 package ru.exlmoto.digest.site.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import ru.exlmoto.digest.site.test.AdministratorAuthenticationFilter;
-import ru.exlmoto.digest.site.test.AdministratorDetailService;
 
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
-	private final AdministratorDetailService administratorDetailService;
-
-	public WebSecurityConfiguration(AdministratorDetailService administratorDetailService) {
-		this.administratorDetailService = administratorDetailService;
-	}
-
-	/*
 	@Bean
 	@Override
 	protected UserDetailsService userDetailsService() {
@@ -30,35 +25,11 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 			.roles("ADMIN")
 			.build();
 		return new InMemoryUserDetailsManager(user);
-	}*/
-
-	public AdministratorAuthenticationFilter authenticationFilter() throws Exception {
-		AdministratorAuthenticationFilter filter = new AdministratorAuthenticationFilter();
-		filter.setAuthenticationManager(authenticationManagerBean());
-		filter.setAuthenticationFailureHandler(failureHandler()); //???
-		return filter;
-	}
-
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.authenticationProvider(authProvider());
-	}
-
-	public AuthenticationProvider authProvider() {
-		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-		provider.setUserDetailsService(administratorDetailService);
-		// provider.setPasswordEncoder(passwordEncoder()); //???
-		return provider;
-	}
-
-	public SimpleUrlAuthenticationFailureHandler failureHandler() {
-		return new SimpleUrlAuthenticationFailureHandler("/ds-auth-login?error=true");
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.addFilterBefore(authenticationFilter(), AdministratorAuthenticationFilter.class)
-			.authorizeRequests()
+		http.authorizeRequests()
 			.antMatchers("/obey/**")
 				.authenticated()
 			.anyRequest()

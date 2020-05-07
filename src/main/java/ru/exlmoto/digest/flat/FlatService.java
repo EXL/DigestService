d4f781a01bb789eeb25506564b01fee779cbe12d
EXL <exlmotodev@gmail.com>
2020-05-07 16:38:43 +0700

    Add api urls

--- src/main/java/ru/exlmoto/digest/flat/FlatService.java
+++ src/main/java/ru/exlmoto/digest/flat/FlatService.java
@@ -1,5 +1,6 @@
 package ru.exlmoto.digest.flat;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import ru.exlmoto.digest.flat.generator.FlatTgHtmlGenerator;
@@ -8,17 +9,35 @@ import ru.exlmoto.digest.flat.generator.FlatTgHtmlGenerator;
 public class FlatService {
 	private final FlatTgHtmlGenerator generator;
 
-	// TEST DATA NOT COMMIT
+	@Value("${flat.cian.slot1}")
+	private String cianUrlOne;
+
+	@Value("${flat.cian.slot2}")
+	private String cianUrlTwo;
+
+	@Value("${flat.n1.slot1}")
+	private String n1UrlOne;
+
+	@Value("${flat.n1.slot2}")
+	private String n1UrlTwo;
 
 	public FlatService(FlatTgHtmlGenerator generator) {
 		this.generator = generator;
 	}
 
-	public String tgHtmlReportCian() {
-		return generator.getTgHtmlReportCian();
+	public String tgHtmlReportCianSlotFirst() {
+		return generator.getTgHtmlReportCian(cianUrlOne);
 	}
 
-	public String tgHtmlReportN1() {
-		return generator.getTgHtmlReportN1();
+	public String tgHtmlReportN1SlotFirst() {
+		return generator.getTgHtmlReportN1(n1UrlOne);
+	}
+
+	public String tgHtmlReportCianSlotSecond() {
+		return generator.getTgHtmlReportCian(cianUrlTwo);
+	}
+
+	public String tgHtmlReportN1SlotSecond() {
+		return generator.getTgHtmlReportN1(n1UrlTwo);
 	}
 }

db957c50b9f427f110a413ad91465046c77528f3
EXL <exlmotodev@gmail.com>
2020-05-09 00:52:55 +0700

    Fix COVID-2019 digit limits on report
    Fix translations

--- src/main/java/ru/exlmoto/digest/covid/generator/CovidTgHtmlGenerator.java
+++ src/main/java/ru/exlmoto/digest/covid/generator/CovidTgHtmlGenerator.java
@@ -109,12 +109,12 @@ public class CovidTgHtmlGenerator {
 	}
 
 	private String generateTable(List<RegionFull> cases, Locale lang) {
-		final int MAX_REGIONS = 70;
+		final int MAX_REGIONS = 65;
 
-		final int CHOP_REGION = 15;
-		final int CHOP_CASES = 12;
-		final int CHOP_RECOV = 10;
-		final int CHOP_DEATH = 9;
+		final int CHOP_REGION = 10;
+		final int CHOP_CASES = 13;
+		final int CHOP_RECOV = 12;
+		final int CHOP_DEATH = 10;
 
 		StringBuilder builder = new StringBuilder("\n\n<pre>");
 		builder.append(filter.arrangeString(locale.i18nW("covid.table.region", lang), CHOP_REGION)).append(" ");
